5afdf25a1944b3c2b5b500dd36f16314bb227d51
Andrea Baravelli <Andrea.Bavarelli@Horsa.it>
2023-07-25 11:43:56 +0200

    firstcommit

---
+++ progettoProva2/Data/progettoProva2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using progettoProva2.Models;
+
+namespace progettoProva2.Data
+{
+    public class progettoProva2Context : DbContext
+    {
+        public progettoProva2Context (DbContextOptions<progettoProva2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<progettoProva2.Models.Job> Job { get; set; } = default!;
+    }
+}
---
+++ progettoProva2/Models/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using progettoProva2.Data;
+
+namespace progettoProva2.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+           
+            using (var context = new progettoProva2Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<progettoProva2Context>>()))
+
+            {
+                if (context.Job.Any())
+                {
+                    return;
+                }
+                context.Job.AddRange(
+                    new Job
+                    {
+                        Name = "Job1"
+                    },
+                    new Job
+                    {
+                        Name= "Job2"
+                    },
+                    new Job
+                    {
+                        Name= "Job3"
+                    });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
